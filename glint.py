d0912144ebda1d8b4f7c48229742575f3e537f41
Cameron Stitt <cameron@castitt.com>
2013-05-17 22:21:14 -0700

    Better commits with line reference

--- glint.py
+++ glint.py
@@ -4,6 +4,7 @@ import tempfile
 import urllib2
 import re
 import os
+import mimetypes
 
 import envoy
 import requests
@@ -12,68 +13,108 @@ from github import Github
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(
-            description="Posts static analysis results to github.")
-    parser.add_argument(
-        '--repo_name', required=True,
-        help="Github repository name in owner/repo format")
-    parser.add_argument('--pull', required=True,
-                         help="The pull request number to run the lint on.")
-    parser.add_argument(
-        '--filenames', nargs="+",
-        help="filenames you want static analysis to be limited to.")
-    parser.add_argument(
-        '--debug', action='store_true',
-        help="Will dump debugging output and won't clean up after itself.")
-    parser.add_argument(
-        '--github-username',
-        required=True,
-        help='Github user to post comments as.')
-    parser.add_argument(
-        '--github-password',
-        required=True,
-        help='Github password for the above user.')
+ACCEPTABLE_TYPES = (
+    u'text/x-python'
+)
 
-    args = parser.parse_args()
-    repo_name = args.repo_name
-    pull = int(args.pull)
+ACCEPTABLE_TYPES_CHOICES = {
+    u'text/x-python': u'python',
+    u'application/javascript': u'javascript'
+}
 
-    credentials = {
-        'user': args.github_username,
-        'password': args.github_password}
 
-    g = Github(credentials['user'], credentials['password'])
+def do_pylint(fle, temp_file, pull, commit):
+    filename = fle.filename
+    cmd = u'flake8 {0}'.format(temp_file.name)
+    result = envoy.run(cmd)
 
+    newline_stripped = result.std_out.replace(u'\n', u'')
+    for line in newline_stripped.split(u"{0}/".format(os.path.dirname(temp_file.name))):
+        if len(line) == 0:
+            continue
+        filename, line_num, column, error = line.split(u':', 3)
+        
+        line_link = u'{0}#L{1}'.format(fle.blob_url, line_num)
+        
+        commit_body = u'Line [{0}]({1}): {2}'.format(line_num, line_link, 
+            error.strip())
+        
+        commit.create_comment(
+            body=commit_body,
+            path=filename,
+            line=int(line_num))
+
+
+def do_lint(g, repo_name, pull_number, credentials):
     repo = g.get_repo(repo_name)
 
-    pull = repo.get_pull(pull)
+    pull = repo.get_pull(pull_number)
+
+    commit = repo.get_commit(pull.head.sha)
 
     files = pull.get_files()
 
-    import pdb; pdb.set_trace()
-    for file in files:
-        fname = re.findall(r'[^\/]+$', file.filename)
+    for fle in files:
+        fname = re.findall(r'[^\/]+$', fle.filename)
         if len(fname) <= 0:
-            #TODO(cam-stitt): Error
             continue
+            
+        block_list = re.findall(u'@@ -\d+,\d+ \+\d+,\d+ @@', fle.patch)
+        
         fname = fname[0]
-        req = requests.get(file.raw_url,
-            auth=(credentials['user'], credentials['password']))
+        req = requests.get(fle.raw_url,
+                           auth=(credentials[u'user'], credentials[u'password']))
         if req.status_code != 200:
             continue
-        tf = open("{0}/{1}".format(tempfile.gettempdir(), fname), "w+")
-        tf.write(req.text)
-        tf.close()
-        cmd = 'pylint {0} --output-format=parseable'.format(tf.name)
-        result = envoy.run(cmd)
-        
-        for line in result.std_out.split("\n{0}".format(tf.name)):
-            if len(line) == 0:
-                continue
-            filename, line_num, error = line.split(':', 2)
-            
-            to_return[filename][line_num].append(error)
-        
-        os.remove(tf.name)
+        temp_file = open(u"{0}/{1}".format(tempfile.gettempdir(), fname), u"w+")
+        temp_file.write(req.text)
+        temp_file.close()
+
+        file_type = mimetypes.guess_type(temp_file.name)
+
+        if file_type[0] in ACCEPTABLE_TYPES:
+            name = ACCEPTABLE_TYPES_CHOICES[file_type[0]]
+            if name == 'python':
+                do_pylint(fle, temp_file, pull, commit)
+
+        os.remove(temp_file.name)
+
+
+if __name__ == u'__main__':
+    """
+    python glint.py --repo_name="cam-stitt/glint" --pull=2 --github-username="secret" --github-password="squirrel"
+    """
+    import argparse
+    parser = argparse.ArgumentParser(
+            description=u"Posts static analysis results to github.")
+    parser.add_argument(
+        u'--repo_name', required=True,
+        help=u"Github repository name in owner/repo format")
+    parser.add_argument(u'--pull', required=True,
+                         help=u"The pull request number to run the lint on.")
+    parser.add_argument(
+        u'--filenames', nargs=u"+",
+        help=u"filenames you want static analysis to be limited to.")
+    parser.add_argument(
+        u'--debug', action=u'store_true',
+        help=u"Will dump debugging output and won't clean up after itself.")
+    parser.add_argument(
+        u'--github-username',
+        required=True,
+        help=u'Github user to post comments as.')
+    parser.add_argument(
+        u'--github-password',
+        required=True,
+        help=u'Github password for the above user.')
+
+    args = parser.parse_args()
+    repo_name = args.repo_name
+    pull_number = int(args.pull)
+
+    credentials = {
+        u'user': args.github_username,
+        u'password': args.github_password}
+
+    g = Github(credentials[u'user'], credentials[u'password'])
+
+    do_lint(g, repo_name, pull_number, credentials)
