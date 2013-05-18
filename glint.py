6e1debe796d6ef3365680830dc2fc9b709b05ea0
Cameron Stitt <cameron@castitt.com>
2013-05-17 23:55:02 -0700

    Finish jshint

--- glint.py
+++ glint.py
@@ -14,13 +14,22 @@ logging.basicConfig()
 log = logging.getLogger(__name__)
 
 ACCEPTABLE_TYPES = (
-    u'text/x-python'
+    u'text/x-python',
+    u'application/javascript'
 )
 
 ACCEPTABLE_TYPES_CHOICES = {
     u'text/x-python': u'python',
     u'application/javascript': u'javascript'
 }
+
+
+def create_comment(commit, body, filename, line_num):
+    print body
+    commit.create_comment(
+        body=body,
+        path=filename,
+        line=int(line_num))
 
 
 def do_pylint(fle, temp_file, pull, commit):
@@ -39,10 +48,11 @@ def do_pylint(fle, temp_file, pull, commit):
         commit_body = u'Line [{0}]({1}): {2}'.format(line_num, line_link, 
             error.strip())
         
-        commit.create_comment(
-            body=commit_body,
-            path=filename,
-            line=int(line_num))
+        create_comment(
+            commit,
+            commit_body,
+            filename,
+            line_num)
 
 
 def do_jshint(fle, temp_file, pull, commit):
@@ -50,12 +60,27 @@ def do_jshint(fle, temp_file, pull, commit):
     filename = fle.filename
     cmd = u'jshint --reporter=reporter.js {0}'.format(temp_file.name)
     result = envoy.run(cmd)
-    
+
     newline_stripped = result.std_out.replace(u'\n', u'')
-    
+
+    for line in newline_stripped.split(u"{0}/".format(os.path.dirname(temp_file.name))):
+        if len(line) == 0:
+            continue
+        filename, line_num, column, error = line.split(u':', 3)
+
+        line_link = u'{0}#L{1}'.format(fle.blob_url, line_num)
+
+        commit_body = u'Line [{0}]({1}): {2}'.format(line_num, line_link,
+            error.strip())
+            
+        create_comment(
+            commit,
+            commit_body,
+            filename,
+            line_num)
 
 
-def do_lint(g, repo_name, pull_number, credentials):
+def do_lint(g, repo_name, pull_number, credentials, filenames):
     repo = g.get_repo(repo_name)
 
     pull = repo.get_pull(pull_number)
@@ -72,6 +97,9 @@ def do_lint(g, repo_name, pull_number, credentials):
         block_list = re.findall(u'@@ -\d+,\d+ \+\d+,\d+ @@', fle.patch)
         
         fname = fname[0]
+        if fname not in filenames:
+            continue
+
         req = requests.get(fle.raw_url,
                            auth=(credentials[u'user'], credentials[u'password']))
         if req.status_code != 200:
@@ -82,7 +110,7 @@ def do_lint(g, repo_name, pull_number, credentials):
 
         file_type = mimetypes.guess_type(temp_file.name)
 
-        if file_type[0] in ACCEPTABLE_TYPES:
+        if unicode(file_type[0]) in ACCEPTABLE_TYPES:
             name = ACCEPTABLE_TYPES_CHOICES[file_type[0]]
             if name == 'python':
                 do_pylint(fle, temp_file, pull, commit)
@@ -105,7 +133,7 @@ if __name__ == u'__main__':
     parser.add_argument(u'--pull', required=True,
                          help=u"The pull request number to run the lint on.")
     parser.add_argument(
-        u'--filenames', nargs=u"+",
+        u'--filenames',
         help=u"filenames you want static analysis to be limited to.")
     parser.add_argument(
         u'--debug', action=u'store_true',
@@ -122,6 +150,8 @@ if __name__ == u'__main__':
     args = parser.parse_args()
     repo_name = args.repo_name
     pull_number = int(args.pull)
+    if args.filenames:
+        filenames = unicode(args.filenames).split(',')
 
     credentials = {
         u'user': args.github_username,
@@ -129,4 +159,4 @@ if __name__ == u'__main__':
 
     g = Github(credentials[u'user'], credentials[u'password'])
 
-    do_lint(g, repo_name, pull_number, credentials)
+    do_lint(g, repo_name, pull_number, credentials, filenames)
